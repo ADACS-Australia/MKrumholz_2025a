51266aa9620dd0d2f14e4d6f22293220ba6489b2
JHu <63333128+JHu-s@users.noreply.github.com>
2025-06-13 07:58:28 +1000

    generate script to submit jobs

--- scripts/generate.py
+++ scripts/generate.py
@@ -37,33 +37,38 @@ re_build = build_temp.render(
 with open("build_all.sh", "w") as f:
     f.write(re_build)
 os.chmod("build_all.sh", os.stat("build_all.sh").st_mode | stat.S_IXUSR)
-print(f"✅ Build script generated: ")
+print("✅ Build script generated: build_all.sh")
 
 # --- Generate job scripts ---
 source_dir = working_dir/timestamp/"quokka"
 
-for test in config["tests"]:
-    for core in test["cores"]:
-        # create a directory for each test job
-        result_dir = str(working_dir/"results"/f"{test['name']}_n{core}")
-        os.makedirs(result_dir, exist_ok=True)
-        rendered = template.render(
-            shell=config["shell"],
-            env_setup_script = config["env"]["script"],
-            source_dir=str(source_dir),
-            build_dir=str(source_dir/"build"),
-            test_in_dir=str(source_dir/"tests"),
-            result_dir = result_dir,
-            test_name=test["name"],
-            target=test["target"],
-            input_file=test["input_file"],
-            cores=core,
-            time_limit=test["time_limit"],
-            memory=test["memory"]
-        )
+with open("submit_jobs.sh", "w") as fjob:
+    for test in config["tests"]:
+        for core in test["cores"]:
+            # create a directory for each test job
+            result_dir = str(working_dir/"results"/f"{test['name']}_n{core}")
+            os.makedirs(result_dir, exist_ok=True)
+            rendered = template.render(
+                shell=config["shell"],
+                env_setup_script = config["env"]["script"],
+                source_dir=str(source_dir),
+                build_dir=str(source_dir/"build"),
+                test_in_dir=str(source_dir/"tests"),
+                result_dir = result_dir,
+                test_name=test["name"],
+                target=test["target"],
+                input_file=test["input_file"],
+                cores=core,
+                time_limit=test["time_limit"],
+                memory=test["memory"]
+            )
 
-        job_name = result_dir + f"/{test['name']}_n{core}.sh"
-        with open(job_name, "w") as f:
-            f.write(rendered)
-        os.chmod(job_name, os.stat(job_name).st_mode | stat.S_IXUSR)
-        print(f"✅ Job script generated: {job_name}")
+            job_name = result_dir + f"/{test['name']}_n{core}.sh"
+            with open(job_name, "w") as f:
+                f.write(rendered)
+            os.chmod(job_name, os.stat(job_name).st_mode | stat.S_IXUSR)
+            print(f"✅ Job script generated: {job_name}")
+            fjob.write(f"sbatch {job_name} \n")
+
+os.chmod("submit_jobs.sh", os.stat("submit_jobs.sh").st_mode | stat.S_IXUSR)
+print("✅ Job submission script generated: submit_jobs.sh")
