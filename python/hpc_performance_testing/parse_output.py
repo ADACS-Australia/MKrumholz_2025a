bf1c01246e51a3567fb34b19d062c94e3378f162
JHu <63333128+JHu-s@users.noreply.github.com>
2025-07-08 11:58:57 +1000

    refactor parser functions

--- python/hpc_performance_testing/parse_output.py
+++ python/hpc_performance_testing/parse_output.py
@@ -1,24 +1,45 @@
+from pathlib import Path
 import re
-from patterns import *
 
+from hpc_performance_testing.utils import validate_path
+from hpc_performance_testing.patterns import *
 
-def get_zone_update_info(outfile):
-    microseconds_per_update = None
-    megaupdates_per_second = None
-    n_process = None
-    with open(outfile) as f:
-        content = f.read()
+class ProfileParser:
+    
+    def __init__(self, profile_file: Path | str ):
+        profile_file = validate_path(profile_file)
+        self._read_file(profile_file)
         
-        match_performance = RE_ZONE_UPDATE_RATE.search(content)
+    def _read_file(self, filename):
+        with open(filename) as f:
+            content = f.read()
+        self.content = content
+
+    def get_zone_update_info(self):
+        microseconds_per_update = None
+        megaupdates_per_second = None
+        n_process = None
+ 
+        match_performance = RE_ZONE_UPDATE_RATE.search(self.content)
 
         if match_performance:
             microseconds_per_update = float(match_performance.group(1))   
             megaupdates_per_second = float(match_performance.group(2))  
 
-        match_np = RE_N_MPI_PROCESS.search(content)
+        match_np = RE_N_MPI_PROCESS.search(self.content)
         if match_np:
             n_process = int(match_np.group(1))
-    print(microseconds_per_update, megaupdates_per_second, n_process)
+        print(microseconds_per_update, megaupdates_per_second, n_process)
+
+    def get_elapse_time(self):
+        elapse_time = None
+        match_et = ELPASE_TIME.search(self.content)
+        if match_et:
+            elapse_time = float(match_et.group(1))
+        print(f"elapse time: {elapse_time}")
+        return elapse_time
 
 if __name__ == "__main__":
-    get_zone_update_info("test_hydro3d_blast_gpu_n8_v2_1683814.out")
+    parser = ProfileParser("test_hydro3d_blast_gpu_n8_v2_1902441.out")
+    parser.get_zone_update_info()
+    parser.get_elapse_time()
--- python/hpc_performance_testing/patterns.py
+++ python/hpc_performance_testing/patterns.py
@@ -5,6 +5,8 @@ RE_ZONE_UPDATE_RATE = re.compile(r'(\d+\.?\d*)\s+μs/zone-update\s+\[(\d+\.?\d*)
 
 RE_N_MPI_PROCESS = re.compile(r'MPI initialized with (\d+) MPI processes') 
 
+ELPASE_TIME = re.compile(r'elapsed time:\s*(.*?)\s*seconds\.')
+
 
 # HPC specific patterns
 
